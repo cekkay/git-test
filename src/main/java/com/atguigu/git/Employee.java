18eee26b9f428183bd7edc9b68b219b718ca339b
cekkay <2225003046@qq.com>
2023-02-28 12:24:16 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/Employee.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @author shi
+ * @Description 相关描述
+ * @create 2023-02-2023/2/28-12:19
+ */
+
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git1");
+        System.out.println("hello Git2");
+    }
+}
